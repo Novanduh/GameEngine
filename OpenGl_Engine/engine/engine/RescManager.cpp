303db229886affa46af92953e414d60b1cbdbb8e
Brandon Pero <toozer1@hotmail.com>
2024-02-27 19:53:30 -0500

    Creating resource header file

--- OpenGl_Engine/engine/engine/RescManager.cpp
+++ OpenGl_Engine/engine/engine/RescManager.cpp
@@ -1,6 +1,21 @@
 //create header file
+#include <string>
+#include <stdlib.h>
+#include "resc.h"
+
+#include <objloader.hpp>
+
+
 
 class RescManager {
+	friend class 
 public:
 
+
+private:
+
 };
+
+void main(void) {
+
+}
---
+++ OpenGl_Engine/engine/engine/resc.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+
+typedef struct ProxObj ProxObj;
+
+struct ProxObj {
+	
+	float proxTrigger; // The maximum distance from the player at which the object is rendered.
+};
+
+// A bindary search tree of objects that appear
+// based on proximity to the player.
+class proxtree {
+
+};
+
+// A collection of 
+class rblock {
+public:
+	-
+};
+
+class RescManager {
+public:
+
+
+private:
+	
+};
