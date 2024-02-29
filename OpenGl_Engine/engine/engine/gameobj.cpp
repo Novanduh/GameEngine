9a7f1cbd7bc791b56acbc02f19a66e9756a286cd
Brandon Pero <toozer1@hotmail.com>
2024-02-29 16:33:18 -0500

    Fixed all the errors!

---
+++ OpenGl_Engine/engine/engine/gameobj.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11d1b28e5eb1aa8938ddc2c857ccaec8d6c75efa5cddbe8876b2da6b698279cf
+size 202
---
+++ OpenGl_Engine/engine/engine/gameobj.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0558ed50b7460729c1fda8ee976a827171030ba21934498676f52478dac69ef
+size 270
