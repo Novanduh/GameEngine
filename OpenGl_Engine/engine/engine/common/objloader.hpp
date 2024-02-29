2cb2d09ead3f789b1392a66e05a32ceaae84ddf2
Novanduh <sampratisinha@gmail.com>
2024-02-29 14:57:26 -0500

    fixed renderer

--- OpenGl_Engine/engine/engine/common/objloader.hpp
+++ OpenGl_Engine/engine/engine/common/objloader.hpp
@@ -1,22 +1,26 @@
 //From https://www.opengl-tutorial.org/download/
 #ifndef OBJLOADER_H
 #define OBJLOADER_H
+namespace std
+{
+#include <cstdlib>
+};
 
 bool loadOBJ(
-	const char * path, 
-	std::vector<glm::vec3> & out_vertices, 
-	std::vector<glm::vec2> & out_uvs, 
-	std::vector<glm::vec3> & out_normals
+	const char* path,
+	std::vector<glm::vec3> &out_vertices,
+	std::vector<glm::vec2> &out_uvs,
+	std::vector<glm::vec3> &out_normals
 );
 
 
 
 bool loadAssImp(
-	const char * path, 
-	std::vector<unsigned short> & indices,
-	std::vector<glm::vec3> & vertices,
-	std::vector<glm::vec2> & uvs,
-	std::vector<glm::vec3> & normals
+	const char* path,
+	std::vector<unsigned short> &indices,
+	std::vector<glm::vec3> &vertices,
+	std::vector<glm::vec2> &uvs,
+	std::vector<glm::vec3> &normals
 );
 
 #endif
