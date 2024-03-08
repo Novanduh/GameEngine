b2cdee3f9bba1689242e56ab0c9d2d60543724c0
Novanduh <sampratisinha@gmail.com>
2024-03-08 11:47:03 -0500

    added light and material

---
+++ OpenGl_Engine/engine/engine/Light.cpp
@@ -0,0 +1,17 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include <vector>
+#include <glm.hpp>
+using namespace glm;
+
+
+
+std::vector<glm::vec3> lightPosition;
+
+std::vector<glm::vec3> lightAmbientValue;
+std::vector<glm::vec3> lightDiffuseValue;
+std::vector<glm::vec3> lightSpecularValue;
+
+std::vector<glm::vec3> ambientProduct;
+std::vector<glm::vec3> diffuseProduct;
+std::vector<glm::vec3> specularProduct;
+
